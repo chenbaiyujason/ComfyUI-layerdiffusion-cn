4fb08babd82b663ffe422648cb4ddf35f66279e5
chenbaiyujason <55756122+chenbaiyujason@users.noreply.github.com>
2024-03-04 19:58:21 +0800

    Update layered_diffusion.py

--- layered_diffusion.py
+++ layered_diffusion.py
@@ -16,7 +16,7 @@ from .lib_layerdiffusion.utils import (
 from .lib_layerdiffusion.models import TransparentVAEDecoder
 
 
-layer_model_root = os.path.join(folder_paths.models_dir, "layer_model")
+layer_model_root = folder_paths.get_folder_paths("layer_models")
 load_layer_model_state_dict = load_torch_file
 
 
